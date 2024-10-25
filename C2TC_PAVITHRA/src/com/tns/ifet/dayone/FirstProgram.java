e8e5ad54401cddb847af7c3a02ecb40f8795201c
Administrator <Administrator@Pavi>
2024-10-25 14:47:46 +0530

    new update

--- C2TC_PAVITHRA/src/com/tns/ifet/dayone/FirstProgram.java
+++ C2TC_PAVITHRA/src/com/tns/ifet/dayone/FirstProgram.java
@@ -3,9 +3,7 @@ package com.tns.ifet.dayone;
 public class FirstProgram {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-//		System.out.println("Hello");
-		System.out.println("Hiii");
+		System.out.println("WELCOME");
 	}
 
 }
